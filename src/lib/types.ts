22388cf413f957a7115e59cf7ebb2b7e0a565ed2
Jonatan Missora <jonatanjmissora@gmail.com>
2025-12-24 15:33:55 -0300

    empezando con el search

---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const searchSchema = z.object({
+    page: z.number().default(1).catch(1),
+    filter: z.string().default("").catch(""),
+    sort: z.enum(["asc", "desc"]).default("asc").catch("asc"),
+});
+
+export type SearchParams = z.infer<typeof searchSchema>
--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,9 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { searchSchema } from '../lib/types'
 
 export const Route = createFileRoute('/search')({
   component: RouteComponent,
+  validateSearch: searchSchema,
+  loaderDeps: ({search}) => ({search}),
+  loader: async ({ deps: {search} }) => {
+    console.log("search", search)
+    return search
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/search"!</div>
+  const {page, filter, sort } = Route.useLoaderData()
+  return <div>Hello "/search"! {page} {filter} {sort}</div>
 }
