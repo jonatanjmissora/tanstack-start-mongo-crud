7b10b390f29ddb603b4275aeace28e05e630a4bc
Jonatan Missora <jonatanjmissora@gmail.com>
2026-01-06 12:09:47 -0300

    biome format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-  
-  return (
-    <section className="flex flex-col items-center justify-center h-screen w-screen bg-blue-900">
-      hola mundo
-    </section>
-  )
+	return (
+		<section className="flex flex-col items-center justify-center h-screen w-screen bg-blue-900">
+			hola mundo
+		</section>
+	);
 }
 
-export default App
+export default App;
