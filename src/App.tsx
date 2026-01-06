85f44277092a4a798919739aa1c24573229744c1
Jonatan Missora <jonatanjmissora@gmail.com>
2026-01-06 11:49:00 -0300

    agregado defaultPendingMs: 0, para no tener que esperar para ver el loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
-import { useRouterContext } from "./lib/use-router-context"
+import { useRouterContext } from "./lib/use-router-context";
 
 const router = createRouter({
-	routeTree,
-	defaultPreload: "intent",
-	defaultStaleTime: 5000,
-	scrollRestoration: true,
-	context: {
-		role: null,
-		login: () => {},
-		logout: () => {},
-		isAdmin: false,
-		isClient: false,
-		isAuthenticated: false,
-	},
-})
+  routeTree,
+  defaultPendingMs: 0,
+  defaultPreload: "intent",
+  defaultStaleTime: 5000,
+  scrollRestoration: true,
+  context: {
+    role: null,
+    login: () => {},
+    logout: () => {},
+    isAdmin: false,
+    isClient: false,
+    isAuthenticated: false,
+  },
+});
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 function App() {
-	const routerContext = useRouterContext()
-	return <RouterProvider router={router} context={routerContext} />
+
+  const routerContext = useRouterContext();
+  return (
+    <RouterProvider router={router} context={routerContext} />
+  )
 }
 
 export default App
